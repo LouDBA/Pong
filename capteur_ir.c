c2ab28e37d6f8c46faad0e5c6d46c98110555534
Xamence <maxence.dominjon@gmail.com>
2022-04-22 12:02:43 +0200

    Création fichier pour les capteurs infrarouges

---
+++ capteur_ir.c
@@ -0,0 +1,10 @@
+/*
+ * capteur_ir.c
+ *
+ *  Created on: 22 avr. 2022
+ *      Author: Maxence
+ */
+
+
+
+
---
+++ capteur_ir.h
@@ -0,0 +1,15 @@
+/*
+ * capteur_ir.h
+ *
+ *  Created on: 22 avr. 2022
+ *      Author: Maxence
+ */
+
+#ifndef CAPTEUR_IR_H_
+#define CAPTEUR_IR_H_
+
+
+
+
+
+#endif /* CAPTEUR_IR_H_ */
