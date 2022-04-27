f4675482d3f0e9bca2446aa5e982e300ed713748
Xamence <maxence.dominjon@gmail.com>
2022-04-27 17:08:14 +0200

    Capteur IR qui fonctionne, le robot tourne à l'approche d'un mur

--- capteur_ir.h
+++ capteur_ir.h
@@ -1,15 +1,8 @@
-/*
- * capteur_ir.h
- *
- *  Created on: 22 avr. 2022
- *      Author: Maxence
- */
-
 #ifndef CAPTEUR_IR_H_
 #define CAPTEUR_IR_H_
 
 
-
-
+//start
+void capteur_ir_start(void);
 
 #endif /* CAPTEUR_IR_H_ */
